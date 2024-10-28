8bb5ac38fb0ae8e39099be86c22cadcafb304cc0
Dkoth <davidokoth857@gmail.com>
2024-10-28 07:30:35 -0700

    Full description

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 --  Script that prints the full description of the table books from the database alx_book_store
 USE alx_book_store;
 
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+SELECT COLUMN_TYPE,COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
 
